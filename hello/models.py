5fcc524b50df0d7c62b8edb4c89d2000a81aba5d
Lucy Wilcox <l.y.wilcox@gmail.com>
2018-02-21 16:46:09 -0500

    adding migrations

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,12 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+class Vehicles(models.Model):
+	full_vin = models.IntegerField()
+	partial_vin = models.IntegerField()
+	v_make = models.CharField(max_length=100)
+	v_model = models.CharField(max_length=100)
+	v_series = models.CharField(max_length=100)
+	v_gvwr = models.CharField(max_length=100)
+	when = models.DateTimeField('date created', auto_now_add=True)
