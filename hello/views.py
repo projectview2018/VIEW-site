aba8f00cd04630390b15417a5d3cf4554b6ff32a
Lucy Wilcox <l.y.wilcox@gmail.com>
2018-02-24 17:48:30 -0500

    added vehicle route

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
+from .models import Greeting, Vehicles
+# from rest_framework.test import APIClient
+# from rest_framework import status
+# from django.core.urlresolvers import reverse
+
 
 # Create your views here.
 def index(request):
@@ -17,4 +21,9 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
+
+def addvehicle(request, fullvin, partialvin, vmake, vmodel, vseries, vgvwr):
+	v = Vehicles(fullvin, partialvin, vmake, vmodel, vseries, vgvwr)
+	v.save()
+
 
