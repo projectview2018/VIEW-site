d98f04f1458985f754aa880c3f5bcbfc9585dfa1
Lucy Wilcox <l.y.wilcox@gmail.com>
2018-02-28 11:10:58 -0500

    add perc to api

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -11,7 +11,7 @@ import hello.views
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-	path('api/v1/<fullvin>/<partialvin>/<vmake>/<vmodel>/<vgvwr>/', hello.views.addvehicle),
+	path('api/v1/<fullvin>/<partialvin>/<vmake>/<vmodel>/<vgvwr>/<perc_vis>', hello.views.addvehicle),
     url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
     path('admin/', admin.site.urls),
