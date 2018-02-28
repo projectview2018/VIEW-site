07394d9c8777c4bd43dbbf12dbfde78b5de052b3
Lucy Wilcox <l.y.wilcox@gmail.com>
2018-02-28 10:42:45 -0500

    update migration

--- hello/models.py
+++ hello/models.py
@@ -10,6 +10,7 @@ class Vehicles(models.Model):
 	vmake = models.CharField(max_length=100)
 	vmodel = models.CharField(max_length=100)
 	vgvwr = models.CharField(max_length=100)
+	perc_vis = models.IntegerField()
 	when = models.DateTimeField('date created', auto_now_add=True)
 
 #https://stackoverflow.com/questions/28598676/django-heroku-error-your-models-have-changes-that-are-not-yet-reflected-in-a-mi
