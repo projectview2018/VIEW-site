7163d5910d7e8a815ad8c4225488e80c2d088901
Lucy Wilcox <l.y.wilcox@gmail.com>
2018-03-13 22:26:50 -0400

    fix typo

--- hello/views.py
+++ hello/views.py
@@ -73,7 +73,7 @@ def getinterestarea(request):
     c = request.json['c']
     d = request.json['d']
     interest_area = find_total_truck_interest_area(angles, c, d)
-    return HttpResponse(interest_area)
+    return JsonResponse({"data": interest_area})
 
 @require_http_methods(["POST"])
 def getblindarea(request, NVPs, angles, DH, c, d):
